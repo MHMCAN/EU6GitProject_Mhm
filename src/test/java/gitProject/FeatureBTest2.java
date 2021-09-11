b3bb5dd048c7a75320b9c14ffe4ea3f93e30b3ff
MHMCAN <mmrcan@gmail.com>
2021-09-11 22:43:37 +0300

    my second commit TO FeatureB branch

--- src/test/java/gitProject/FeatureBTest2.java
+++ src/test/java/gitProject/FeatureBTest2.java
@@ -3,5 +3,6 @@ package gitProject;
 public class FeatureBTest2 {
     public static void main(String[] args) {
         System.out.println("i am developing some new cool stuff");
+        System.out.println("hay mede gora ...");
     }
 }
