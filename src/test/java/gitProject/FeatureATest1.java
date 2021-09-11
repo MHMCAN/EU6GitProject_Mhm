0cd6ccdc476616028d0feb1e58d595d4304ea9fb
MHMCAN <mmrcan@gmail.com>
2021-09-11 22:35:23 +0300

    my Second commit TO FeatureA branch

--- src/test/java/gitProject/FeatureATest1.java
+++ src/test/java/gitProject/FeatureATest1.java
@@ -3,5 +3,6 @@ package gitProject;
 public class FeatureATest1 {
     public static void main(String[] args) {
         System.out.println("I made some awesome work on my featureA");
+        System.out.println("more more");
     }
 }
